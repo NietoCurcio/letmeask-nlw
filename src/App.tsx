f8d32c56c5f7652c417b346a5dd02e64d46a94ac
Felipe Curcio <felipe_nieto010@hotmail.com>
2021-06-22 20:49:32 -0300

    firebase authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { createContext, useState, useEffect } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom'
+import { auth, firebase } from './services/firebase'
+
+import { AuthContextProvider } from './contexts/AuthContextProvider'
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
-  );
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App

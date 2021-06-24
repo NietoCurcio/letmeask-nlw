0780047afb20b6667ee4ec17a3b085102e10ad73
Felipe Curcio <felipe_nieto010@hotmail.com>
2021-06-24 00:08:00 -0300

    like button

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,56 @@
+import { useParams } from 'react-router-dom'
+import { Button } from '../components/Button'
+import logoImg from '../assets/images/logo.svg'
+
+import '../styles/room.scss'
+import { RoomCode } from '../components/RoomCode'
+import { useState } from 'react'
+import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
+import { Question } from '../components/Question'
+import { useRoom } from '../hooks/useRoom'
+
+type RoomParams = {
+  id: string
+}
+
+export const AdminRoom = () => {
+  const { user } = useAuth()
+  const params = useParams<RoomParams>()
+  const [newQuestion, setNewQuestion] = useState('')
+  const roomId = params.id
+  const { title, questions } = useRoom(roomId)
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="letmeask" />
+          <div>
+            <RoomCode code={roomId} />
+            <Button isOutlined>Encerrar sala</Button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+        </div>
+
+        <div className="question-list">
+          {questions.map((question) => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              />
+            )
+          })}
+        </div>
+      </main>
+    </div>
+  )
+}

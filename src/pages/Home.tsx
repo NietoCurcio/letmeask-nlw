a60ee1daaf57427d7037e5e7262c8f9e4ff97f89
Felipe Curcio <felipe_nieto010@hotmail.com>
2021-06-24 23:47:42 -0300

    responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -51,7 +51,7 @@ export function Home() {
           src={illustrationImg}
           alt="Ilustração simbolizando perguntas e respostas"
         />
-        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
         <p>Tire as dúvidas de sua audiência em tempo real</p>
       </aside>
       <main>
